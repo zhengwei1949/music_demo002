7a9fb4319bad2e6f415776582db7e9014e79da96
zhengwei1949 <zhengwei1949@qq.com>
2017-05-16 10:47:55 +0800

    使用模板引擎渲染首页

--- index.js
+++ index.js
@@ -2,6 +2,72 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
+var template = require('art-template');
+var arr = [
+    {
+        "id": 1,
+        "title": "富士山下",
+        "singer": "陈奕迅",
+        "music": "陈奕迅 - 富士山下.mp3",
+        "poster": "陈奕迅.jpg"
+    },
+    {
+        "id": 2,
+        "title": "石头记",
+        "singer": "达明一派",
+        "music": "达明一派 - 石头记.mp3",
+        "poster": "达明一派.jpg"
+    },
+    {
+        "id": 3,
+        "title": "青城山下白素贞",
+        "singer": "好妹妹乐队",
+        "music": "好妹妹乐队 - 青城山下白素贞.mp3",
+        "poster": "好妹妹乐队.jpg"
+    },
+    {
+        "id": 4,
+        "title": "友情岁月",
+        "singer": "黄耀明",
+        "music": "黄耀明 - 友情岁月.mp3",
+        "poster": "黄耀明.jpg"
+    },
+    {
+        "id": 5,
+        "title": "梦里水乡",
+        "singer": "江珊",
+        "music": "江珊 - 梦里水乡.mp3",
+        "poster": "江珊.jpg"
+    },
+    {
+        "id": 6,
+        "title": "Blowing In The Wind",
+        "singer": "南方二重唱",
+        "music": "南方二重唱 - Blowing In The Wind.mp3",
+        "poster": "南方二重唱.jpg"
+    },
+    {
+        "id": 7,
+        "title": "女儿情",
+        "singer": "万晓利",
+        "music": "万晓利 - 女儿情.mp3",
+        "poster": "万晓利.jpg"
+    },
+    {
+        "id": 8,
+        "title": "王馨平",
+        "singer": "别问我是谁",
+        "music": "王馨平 - 别问我是谁.mp3",
+        "poster": "王馨平.jpg"
+    },
+    {
+        "id": 9,
+        "title": "五环之歌",
+        "singer": "岳云鹏",
+        "music": "岳云鹏,MC Hotdog - 五环之歌.mp3",
+        "poster": "岳云鹏.jpg"
+    }
+];
 
 // console.log(mime.lookup('/public/js/1.js'));
 // console.log(mime.lookup('/public/css/bootstrap.css'));
@@ -17,12 +83,20 @@ server.on('request',function(req,res){
     // res.write('你请求的路径是:'+req.url+',你请求的方法是:'+req.method);
     if(url === '/'){
         // res.write('你访问的是首页');
-        fs.readFile(path.join(__dirname,'views/index.html'),function(err,data){
-            if(err)throw err;
-            res.writeHead(200,{'Content-Type':'text/html;charset=utf8'});
-            res.write(data);
-            res.end();
+        var html = template(path.join(__dirname,'views/index.html'), {
+            arr:arr
         });
+        res.writeHead(200,{'Content-Type':'text/html;charset=utf8'});
+        res.write(html);
+        res.end();
+        // console.log(html);
+
+        // fs.readFile(path.join(__dirname,'views/index.html'),function(err,data){
+        //     if(err)throw err;
+        //     res.writeHead(200,{'Content-Type':'text/html;charset=utf8'});
+        //     res.write(data);
+        //     res.end();
+        // });
     }else if(url === '/add' && method === 'GET'){
         // res.write('你访问的添加音乐,get');
         // res.end();
